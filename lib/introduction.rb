d912fadb431733f0999503b6a3ac7afe2bf0d981
evan kruger <ekruger713@gmail.com>
2020-01-03 09:30:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("evan", "ruby_boiiiii")
+introduction_with_language("evan", "ruby")
 
 
 def introduction_with_language_optional (name, language = "code")
